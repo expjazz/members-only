9b33b1973f353f28624e1bc5866748623168e37f
Expedito Andrade <expeditojazz@gmail.com>
2020-05-21 05:57:50 -0300

    Added devise. Added bootstrap and bootstrap views. Added Post model, controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/index'
+  root 'welcome#index'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
