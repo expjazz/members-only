d45115fb7b31abadf725d16b5835b6929336e796
frankopkusianwar <okirorfrank3@gmail.com>
2020-05-21 17:13:52 +0300

    add comments model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,17 @@
 class CommentsController < ApplicationController
   def create
+    @comment = Comment.new(comment_params)
+    byebug
+    @comment.user_id = current_user
+    @comment.post_id = params[:post_id]
+    if @comment.save
+      redirect_to post_path(params[:id])
+    else
+      render :new
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 end
