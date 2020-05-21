03a86d3fed907aaa1e01d690973d98bcae00a16c
Expedito Andrade <expeditojazz@gmail.com>
2020-05-21 07:09:21 -0300

    Added username to devise. Added restrictions to the navbar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,25 @@
 class PostsController < ApplicationController
+  before_action :user_signed_in?, only: %i[new create]
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = 'Your post was created successfully'
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def index
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'posts/index'
   root 'welcome#index'
   devise_for :users
+  resources :users, only: [:show]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
